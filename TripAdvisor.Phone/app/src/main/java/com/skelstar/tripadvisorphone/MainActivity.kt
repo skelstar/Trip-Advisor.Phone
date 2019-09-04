7cc3c07d98efe8ba745a594d7e294efc08649dd7
Sean <skelstar@gmail.com>
2019-09-04 16:36:31 +1200

    Scan works
    - shows a list of BLE devices when refresh is clicked

--- TripAdvisor.Phone/app/src/main/java/com/skelstar/tripadvisorphone/MainActivity.kt
+++ TripAdvisor.Phone/app/src/main/java/com/skelstar/tripadvisorphone/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.skelstar.tripadvisorphone
 
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.skelstar.android.notificationchannels.NotificationHelper
 
@@ -14,9 +18,14 @@ import org.jetbrains.anko.toast
 class MainActivity : AppCompatActivity() {
 
     private lateinit var helper: NotificationHelper
+
     var m_bluetoothAdapter: BluetoothAdapter? = null
     lateinit var m_pairedDevices: Set<BluetoothDevice>
     val REQUEST_ENABLE_BLUETOOTH = 1
+
+    companion object {
+        private val TRIP_NOTIFY_ID = 1100
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,40 +34,69 @@ class MainActivity : AppCompatActivity() {
 
         helper = NotificationHelper(this)
 
-        btnNotify.setOnClickListener { view ->
+        btnNotify.setOnClickListener { _ ->
             sendTripNotification(TRIP_NOTIFY_ID, "Your trip")
         }
 
-        select_device_refresh.setOnClickListener()
-
         m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-
         if(m_bluetoothAdapter == null) {
-            toast("Doesn't support bluetooth")
+            toast("this device doesn't support bluetooth")
             return
         }
-
+        if(!m_bluetoothAdapter!!.isEnabled) {
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
+        }
         // https://www.youtube.com/watch?v=Oz4CBHrxMMs&t=1752s (at 25:57)
 
-//        if (!m_bluetoothAdapter.isEnabled)
+        select_device_refresh.setOnClickListener{ pairedDeviceList() }
     }
 
     private fun sendTripNotification(id: Int, title: String) {
+
         when (id) {
             TRIP_NOTIFY_ID -> helper.notify(id, helper.getTripNotification())
         }
     }
 
     private fun pairedDeviceList() {
+        Log.i("pairedDevices", "pairedDeviceLIst")
+        m_pairedDevices = m_bluetoothAdapter!!.bondedDevices
+        val list : ArrayList<BluetoothDevice> = ArrayList()
 
+        if (!m_pairedDevices.isEmpty()) {
+            for (device: BluetoothDevice in m_pairedDevices) {
+                list.add(device)
+                Log.i("device", ""+device)
+            }
+        } else {
+            toast("no paired bluetooth devices found")
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        select_device_list.adapter = adapter
+        select_device_list.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            val device: BluetoothDevice = list[position]
+//            val address: String = device.address
+
+//            val intent = Intent(this, ControlActivity::class.java)
+//            intent.putExtra(EXTRA_ADDRESS, address)
+//            startActivity(intent)
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-    }
+        if (requestCode == REQUEST_ENABLE_BLUETOOTH) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (m_bluetoothAdapter!!.isEnabled) {
+                    toast("Bluetooth has been enabled")
+                } else {
+                    toast("Bluetooth has been disabled")
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                toast("Bluetooth enabling has been canceled")
+            }
+        }    }
 
-
-    companion object {
-        private val TRIP_NOTIFY_ID = 1100
-    }
 }
