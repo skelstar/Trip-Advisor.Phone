6c3c6385ed563137d2ac2e7fe2f786faff8aa66d
Sean <skelstar@gmail.com>
2019-09-10 19:20:06 +1200

    Copied code from https://github.com/appsinthesky/Kotlin-Bluetooth/blob/master/app/src/main/java/com/appsinthesky/bluetoothtutorial/ControlActivity.kt

--- TripAdvisor.Phone/app/src/main/java/com/skelstar/tripadvisorphone/MainActivity.kt
+++ TripAdvisor.Phone/app/src/main/java/com/skelstar/tripadvisorphone/MainActivity.kt
@@ -1,26 +1,22 @@
 package com.skelstar.tripadvisorphone
 
 import android.app.Activity
-import android.bluetooth.*
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothSocket
+import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import android.os.Handler
+import android.os.*
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.skelstar.android.notificationchannels.NotificationHelper
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
 import java.util.*
-import kotlin.concurrent.schedule
-import android.os.Looper
-import android.bluetooth.BluetoothGattCharacteristic
-import android.bluetooth.BluetoothGatt
-import android.os.Message
-import android.widget.Toast
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
-import com.skelstar.android.notificationchannels.createTripNotification
-import org.jetbrains.anko.ctx
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,17 +26,23 @@ class MainActivity : AppCompatActivity() {
     var m_bluetoothAdapter: BluetoothAdapter? = null
     lateinit var m_pairedDevices: Set<BluetoothDevice>
     val REQUEST_ENABLE_BLUETOOTH = 1
-    var mBluetoothGatt:BluetoothGatt ?= null
+    var mBluetoothGatt: BluetoothGatt? = null
 
-    val deviceESP32DevUUID:String = "80:7D:3A:C5:6A:36"
-    val deviceOfInterestUUID2:String = "58:B1:0F:7A:FF:B1"
+    val deviceESP32DevUUID: String = "80:7D:3A:C5:6A:36"
+    val deviceOfInterestUUID2: String = "58:B1:0F:7A:FF:B1"
     val deviceM5StackUUID = "30:AE:A4:4F:A5:2A"
 
     var trip: TripData = TripData(volts = 0f, amphours = 0f, distance = 0f)
+    var mHandler: Handler? = null // Our main handler that will receive callback notifications
 
-    lateinit var mHandler: Handler
 
     companion object {
+        var m_myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+        var m_bluetoothSocket: BluetoothSocket? = null
+        //        lateinit var m_progress: ProgressDialog
+        lateinit var m_bluetoothAdapter: BluetoothAdapter
+        var m_isConnected: Boolean = false
+        lateinit var m_address: String
         val TRIP_NOTIFY_ID = 1100
     }
 
@@ -49,128 +51,123 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        btnConnect.setOnClickListener { _ ->
-            bleConnect()
-        }
-
-        mHandler = object : Handler(Looper.getMainLooper()) {
-            override fun handleMessage(msg: Message) {
-                super.handleMessage(msg)
-                Log.i("Handler", msg.obj as String)
-                toast(msg.obj as String)
-            }
-        }
-
-        m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        if(m_bluetoothAdapter == null) {
-            Toast.makeText(this, "this device doesn't support bluetooth", Toast.LENGTH_SHORT).show()
-            return
-        }
-        if(!m_bluetoothAdapter!!.isEnabled) {
-            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
-        }
-
-        bleConnect()
+        ConnectToDevice(this).execute()
     }
 
-    fun bleConnect() {
-
-        val deviceOfInterest = m_bluetoothAdapter?.getRemoteDevice(deviceESP32DevUUID)    // findTheDeviceOfInterest()
-        if (deviceOfInterest != null) {
-            mBluetoothGatt = deviceOfInterest.connectGatt(this, false, mBleGattCallBack)
-            if (mBluetoothGatt != null) {
-                Log.i("ble", "mBluetoothGatt != null")
+    private fun sendCommand(input: String) {
+        if (m_bluetoothSocket != null) {
+            try{
+                m_bluetoothSocket!!.outputStream.write(input.toByteArray())
+            } catch(e: IOException) {
+                e.printStackTrace()
             }
         }
     }
+
+    private fun disconnect() {
+        if (m_bluetoothSocket != null) {
+            try {
+                m_bluetoothSocket!!.close()
+                m_bluetoothSocket = null
+                m_isConnected = false
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        finish()
+    }
+
+    private class ConnectToDevice(c: Context) : AsyncTask<Void, Void, String>() {
+        private var connectSuccess: Boolean = true
+        private val context: Context
+
+        init {
+            this.context = c
+        }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+//            m_progress = ProgressDialog.show(context, "Connecting...", "please wait")
+        }
+
+        override fun doInBackground(vararg p0: Void?): String? {
+            try {
+                if (m_bluetoothSocket == null || !m_isConnected) {
+                    m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+                    val device: BluetoothDevice = m_bluetoothAdapter.getRemoteDevice(m_address)
+                    m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(m_myUUID)
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
+                    m_bluetoothSocket!!.connect()
+                }
+            } catch (e: IOException) {
+                connectSuccess = false
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if (!connectSuccess) {
+                Log.i("data", "couldn't connect")
+            } else {
+                m_isConnected = true
+            }
+//            m_progress.dismiss()
+        }
+    }
+//
+//    fun bleConnect() {
+//
+//        object : Thread() {
+//            override fun run() {
+//                var fail = false
+//
+//                val device = m_bluetoothAdapter?.getRemoteDevice(deviceESP32DevUUID)
+//                Log.i("BLE", "Connecting bluetooth")
+//
+//                try {
+//                    mBTSocket = createBluetoothSocket(device!!)
+//                } catch (e: IOException) {
+//                    fail = true
+//                    Toast.makeText(baseContext, "Socket creation failed", Toast.LENGTH_SHORT).show()
+//                }
+//
+//                // Establish the Bluetooth socket connection.
+//                try {
+//                    Log.i("bleConnect", "Trying to connect")
+//                    mBTSocket?.connect()
+//                } catch (e: IOException) {
+//                    try {
+//                        fail = true
+//                        mBTSocket?.close()
+//                        mHandler?.obtainMessage(CONNECTING_STATUS, -1, -1)
+//                            ?.sendToTarget()
+//                    } catch (e2: IOException) {
+//                        //insert code to deal with this
+//                        Toast.makeText(baseContext, "Socket creation failed", Toast.LENGTH_SHORT)
+//                            .show()
+//                    }
+//
+//                }
+//
+//                if (fail == false) {
+//                    Log.i("bleConnect", "Didn't fail")
+//                    mConnectedThread = ConnectedThread(mBTSocket!!)
+//                    mConnectedThread.start()
+//
+//                    mHandler?.obtainMessage(CONNECTING_STATUS, 1, -1, name)?.sendToTarget()
+//                }
+//            }
+//        }.start()
+//    }   // bleConnect()
+
 
     fun toastAsync(message: String) {
-        val message = mHandler.obtainMessage(1, message)
-        message.sendToTarget()
+        val message = mHandler?.obtainMessage(1, message)
+        message?.sendToTarget()
     }
 
-    private val mBleGattCallBack: BluetoothGattCallback by lazy {
-        object : BluetoothGattCallback(){
-
-            override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
-                super.onConnectionStateChange(gatt, status, newState)
-                Log.i("ble", "onConnectionStateChange: ${DeviceProfile.getStateDescription(newState)} = ${DeviceProfile.getStatusDescription(status)}")
-                if(newState == BluetoothProfile.STATE_CONNECTED){
-                    Timer().schedule(1000){
-                        gatt!!.requestMtu(128)  // bigger packet size
-                        mBluetoothGatt?.discoverServices()
-                        toastAsync("Connected")
-                    }
-                }
-                else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                    toastAsync("Disconnected!")
-                }
-            }
-
-            override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
-                //BLE service discovery complete
-                super.onServicesDiscovered(gatt, status)
-
-                Log.i("BLE", "Services discovered!")
-
-                val characteristic = getCharacteristic(gatt!!)
-
-                Timer().schedule(200) {
-                    gatt?.setCharacteristicNotification(characteristic!!, true)
-                    Log.i("BLE", "setCharacteristicNotification")
-                }
-
-                if (characteristic != null) {
-                    Timer().schedule(200) {
-                        enableNotification(gatt, characteristic)
-                        Log.i("BLE", "enableNotification")
-                    }
-                }
-            }
-
-            override fun onCharacteristicRead(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?, status: Int) {
-                super.onCharacteristicRead(gatt, characteristic, status)
-                Log.i("ble","onCharacteristicRead: value ${characteristic?.getStringValue(0)}")
-            }
-
-            override fun onCharacteristicChanged(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?) {
-                super.onCharacteristicChanged(gatt, characteristic)
-//                Log.i("ble","onCharacteristicChanged: value ${characteristic?.getStringValue(0)}")
-
-                val data = String(characteristic?.value!!)
-                val mapper = jacksonObjectMapper()
-                trip = mapper.readValue(data)
-
-                createTripNotification(ctx,
-                    TRIP_NOTIFY_ID,
-                    "batt: ${trip.volts}v",
-                    "batt: ${trip.volts}v \n" +
-                            "amphours: ${trip.amphours}AH\n" +
-                            "distance: ${trip.distance}km")
-                Log.i("ble","onCharacteristicChanged: value ${characteristic?.getStringValue(0)}")
-            }
-        }
-    }
-
-    fun toast(message:CharSequence) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_ENABLE_BLUETOOTH) {
-            if (resultCode == Activity.RESULT_OK) {
-                if (m_bluetoothAdapter!!.isEnabled) {
-                    toast("Bluetooth has been enabled")
-                } else {
-                    toast("Bluetooth has been disabled")
-                }
-            } else if (resultCode == Activity.RESULT_CANCELED) {
-                toast("Bluetooth enabling has been canceled")
-            }
-        }
-    }
-}
+}   // end class
 
 
